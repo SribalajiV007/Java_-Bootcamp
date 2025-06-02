483ef8cf3427c943a9ea9822c6b4e31677f06a3b
Sri Balaji V <sribalajihema2003@gmail.com>
2025-06-02 15:33:23 +0530

    interview based folder added

---
+++ 3_Loops_conditions/interviewbased/reversenum.java
@@ -0,0 +1,26 @@
+package interviewbased;
+import java.util.Scanner;
+
+public class reversenum {
+    public static void main(String[] args) {
+        Scanner  s = new Scanner(System.in);
+
+        System.out.print("Enter a number to reverse: ");
+        int num = s.nextInt();
+        int reverse =0;
+
+        while(num!=0){
+            int last = num %10;
+            reverse =reverse * 10+ last;
+            num = num/10;
+        }
+
+        System.out.println("The reverse of give number is: "+reverse);
+
+
+    }
+}
+//input = 123
+// 3 * 10= 0 + 3 =3
+ // 3*10 =30+2
+ // 32*10 =320+1 = 321
