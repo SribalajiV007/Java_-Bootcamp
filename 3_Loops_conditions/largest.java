c1379daaf91ac5935360acc6dc67ff5a80218049
Sri Balaji V <sribalajihema2003@gmail.com>
2025-06-09 23:06:50 +0530

    Continents file added

--- 3_Loops_conditions/largest.java
+++ 3_Loops_conditions/largest.java
@@ -2,27 +2,12 @@ import java.util.Scanner;
 
 
 public class largest {
-    public static void main(String[]args){
-        Scanner s = new Scanner(System.in);
-        
-        System.out.print("Enter a number: ");
-        int a = s.nextInt();
+   public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        int b = s.nextInt();
-
-        System.out.print("Enter a number: ");
-        int c = s.nextInt();
-
-        int max = a;
-
-        if(b > max){
-          max = b;
-        }
-        if(c > max){
-          max=c;
-        }
+       s.ne
+   }
 
         System.out.println("The largest number is: "+max);
     }
-}
+
