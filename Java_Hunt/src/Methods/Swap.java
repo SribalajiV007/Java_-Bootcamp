5b997b90bc7064e5b1c336f2ba949557d60048fe
Sri Balaji V <sribalajihema2003@gmail.com>
2025-06-19 21:33:44 +0530

    change value added

--- Java_Hunt/src/Methods/Swap.java
+++ Java_Hunt/src/Methods/Swap.java
@@ -2,9 +2,12 @@ package Methods;
 
 public class Swap {
     public static void main(String[] args) {
-
+      String naame ="boopathi";
       swap1(2,1);
+      System.out.println(swap2(naame));
     }
+
+
 
     static void swap1(int a,int b){
         System.out.println("a: "+a+" b: "+b);
@@ -12,5 +15,8 @@ public class Swap {
         b = a-b;
         a = a-b;
         System.out.println("a: "+a+" b: "+b);
+        }
+    static String swap2(String naame){
+        return "bala";
     }
 }
