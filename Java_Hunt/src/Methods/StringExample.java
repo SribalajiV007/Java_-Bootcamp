e868c7f883aaf036b78cf770ddd5bec410a86cf5
Sri Balaji V <sribalajihema2003@gmail.com>
2025-06-19 20:01:02 +0530

    String added

---
+++ Java_Hunt/src/Methods/StringExample.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class StringExample {
+    public static void main(String[] args) {
+//        String res = greeting();
+        System.out.println(greeting());
+    }
+
+    static String greeting(){
+        String greet = "Namaste!";
+        return greet;
+    }
+}
