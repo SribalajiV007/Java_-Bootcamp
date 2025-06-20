6f476967eb3c65cb224060abdecdea48b6a2dbc3
Sri Balaji V <sribalajihema2003@gmail.com>
2025-06-20 09:14:19 +0530

    prime added

---
+++ Java_Hunt/src/Methods/Prime.java
@@ -0,0 +1,31 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = s.nextInt();
+
+        if(isPrime(number)){
+            System.out.println(number+" is a Prime Number");
+        }else{
+            System.out.println(number+" is not a Prime Number");
+        }
+        s.close();
+    }
+
+    public static boolean isPrime(int num) {
+        if(num <= 1){
+            return false;
+        }
+        for(int i=2; i * i <= num;i++){
+            if(num %i ==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
