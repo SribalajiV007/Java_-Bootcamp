da40212700c9633656fc0626f33ff6edea96443f
Sri Balaji V <sribalajihema2003@gmail.com>
2025-06-30 23:07:03 +0530

    maximum Array added

---
+++ Java_Hunt/src/Array/InterviewQues/maxArr.java
@@ -0,0 +1,29 @@
+package Array.InterviewQues;
+
+public class maxArr {
+    public static void main(String[] args) {
+        int[] arr = {3, 23, 45, 34, 41, 7, 9};
+        System.out.println(max(arr));
+        System.out.println(maxRange(arr,3,5));
+    }
+
+     static int max(int[] arr){
+         int maxVal = arr[0];
+         for (int i = 0; i < arr.length; i++) {
+             if(arr[i] > maxVal){
+                 maxVal = arr[i];
+             }
+         }
+         return maxVal;
+     }
+
+    static int maxRange(int[] arr, int start, int end){
+        int maxVal = arr[0];
+        for (int i = start; i < end; i++) {
+            if(arr[i] > maxVal){
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+}
