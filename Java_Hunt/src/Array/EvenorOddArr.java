11dcd565885d788a643a2d3176c688dbea4f91e6
Sri Balaji V <sribalajihema2003@gmail.com>
2025-07-01 22:39:15 +0530

    evenorodd of array added

--- Java_Hunt/src/Array/EvenorOddArr.java
+++ Java_Hunt/src/Array/EvenorOddArr.java
@@ -1,4 +1,21 @@
 package Array;
 
 public class EvenorOddArr {
+    public static void main(String[]args){
+        int[] arr = {23,34,54,35,67,45,78};
+        String even= " ";
+        String odd = " ";
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] %2 == 0){
+                even += arr[i]+", ";
+            } else if (arr[i] %2 != 0) {
+                odd += arr[i]+", ";
+            }
+        }
+        System.out.println("Even numbers: "+even);
+        System.out.println("Odd numbers: "+odd);
+
+    }
 }
