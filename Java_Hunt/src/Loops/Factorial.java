e802cc8ccb33070a6b7ffe3a5ddeb860ccc1bb57
Sri Balaji V <sribalajihema2003@gmail.com>
2025-07-16 22:29:37 +0530

    arr and strings added

--- Java_Hunt/src/Loops/Factorial.java
+++ Java_Hunt/src/Loops/Factorial.java
@@ -1,15 +1,19 @@
 package Loops;
 
-public class Factorial {
-    public static void main(String[] args) {
-        int n = 3;
-        int fact = 1;
+import java.util.Scanner;
 
-        for(int i =1;i <= n; i++){
-            fact = fact*i;
-        }
-        System.out.println(fact);
-    }
+public class Factorial {
+  public static void main(String[]args){
+      Scanner s = new Scanner(System.in);
+      System.out.print("Enter a number: ");
+      int n = s.nextInt();
+      int factorial = 1;
+
+      for(int i = 1; i<=n; i++){
+          factorial = factorial * i;
+      }
+      System.out.println(factorial);
+  }
 }
 /*
 *  n= 5;
