f77b466bebd4dbd1883380ce5fb399c47c6be376
Sri Balaji V <sribalajihema2003@gmail.com>
2025-07-20 19:24:10 +0530

    main4 added

---
+++ Java_Hunt/src/Inheritance/Main4.java
@@ -0,0 +1,30 @@
+package Inheritance;
+
+class Person{
+    String name = "Sri balaji";
+
+    void showName(){
+        System.out.println("Name: "+name);
+    }
+}
+
+class Student extends Person{
+    String name = "Kamal";
+    String rollno = "22BCA39";
+
+    void showName(){
+        System.out.println("Name: "+name);
+    }
+    void showRollNo(){
+        System.out.println("Roll No: "+rollno);
+    }
+}
+
+public class Main4 {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        s.showName();
+        s.showRollNo();
+    }
+}
