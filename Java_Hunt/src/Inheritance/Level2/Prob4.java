151b2293d5c3cd5a66563ed397260588e9cda402
Sri Balaji V <sribalajihema2003@gmail.com>
2025-07-20 20:08:03 +0530

    prob 4 added

---
+++ Java_Hunt/src/Inheritance/Level2/Prob4.java
@@ -0,0 +1,29 @@
+package Inheritance.Level2;
+
+class Person{
+    String name;
+    Person(String name) {
+        this.name = name;
+    }
+}
+
+class Student extends Person{
+    int marks;
+    Student(String name,int marks){
+        super(name);
+        this.marks = marks;
+    }
+
+    void display(){
+        System.out.println("Name: "+name+", Marks: "+marks);
+    }
+}
+
+public class Prob4 {
+    public static void main(String[] args) {
+        Student s1 = new Student("Balaji",100);
+        Student s2 = new Student("Mohan",99);
+        s1.display();
+        s2.display();
+    }
+}
