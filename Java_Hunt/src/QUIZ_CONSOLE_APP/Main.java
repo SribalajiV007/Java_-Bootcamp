a373492e23abdf72542d3003e19da4fb362ebde1
Sri Balaji V <sribalajihema2003@gmail.com>
2026-01-05 23:09:12 +0530

    Quiz Application done

--- Java_Hunt/src/QUIZ_CONSOLE_APP/Main.java
+++ Java_Hunt/src/QUIZ_CONSOLE_APP/Main.java
@@ -3,6 +3,7 @@ package QUIZ_CONSOLE_APP;
 public class Main {
     public static void main(String[]args){
         QuestionService service = new QuestionService();
-        service.displayQuestions();
+        service.PlayQuiz();
+        service.printScore();
     }
 }
--- Java_Hunt/src/QUIZ_CONSOLE_APP/QuestionService.java
+++ Java_Hunt/src/QUIZ_CONSOLE_APP/QuestionService.java
@@ -1,8 +1,11 @@
 package QUIZ_CONSOLE_APP;
 
+import java.util.Scanner;
+
 public class QuestionService {
 
     Question[] questions = new Question[5];
+    String selection[] = new String[5];
 
     public QuestionService() {
         questions[0] = new Question(1,"Size of int: ","2","6","4","8","4");
@@ -13,11 +16,40 @@ public class QuestionService {
 
     }
 
-    public void displayQuestions(){
+    public void PlayQuiz(){
 
+        int i =0;
         for (Question q : questions){
+            System.out.println("Question no. : "+q.getId());
             System.out.println(q.getQuestion());
+            System.out.println(q.getOpt1());
+            System.out.println(q.getOpt2());
+            System.out.println(q.getOpt3());
+            System.out.println(q.getOpt4());
+            Scanner s = new Scanner(System.in);
+            selection[i] = s.nextLine();
+            i++;
         }
+
+        for(String s : selection){
+            System.out.println(s);
+        }
+    }
+
+    public void printScore() {
+        int score =0;
+
+        for(int i=0; i<questions.length; i++){
+            Question ques = questions[i];
+            String actualAnswer = ques.getAnswer();
+            String userAnswer = selection[i];
+
+            if(actualAnswer.equals(userAnswer)){
+                score++;
+            }
+        }
+
+        System.out.println("Your score is : "+score);
     }
 }
 
